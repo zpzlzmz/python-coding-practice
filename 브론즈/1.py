69a040f0d42cdffad382fb0a6884cfadeb4ea496
zpzlzmz <48462762+zpzlzmz@users.noreply.github.com>
2022-08-25 08:42:16 -0700

    Delete 1.py

--- 브론즈/1.py
+++
@@ -1,9 +0,0 @@
-def twoSum(nums: list[int], target: int) -> list[int]:
-     for i in range(len(nums)):
-         for j in range(len(nums)-1):
-            if(nums[i]+nums[j]) == target:
-                print("[",i,",",j,"]",sep="")
-                return
-
-
-twoSum([2,7,11,15],9)
