36b937ba68fa041860b91213cb9a577962d14cca
zpzlzmz <48462762+zpzlzmz@users.noreply.github.com>
2022-08-25 08:41:24 -0700

    Delete 19.py

--- 19.py
+++
@@ -1,9 +0,0 @@
-a = []
-
-for _ in range(int(input())):
-    n,d,m,y = input().split()
-
-    a.append([n,int(d),int(m),int(y)])
-a.sort(key = lambda x:(x[3],x[2],x[1]))
-print(a[4][0])
-print(a[0][0])
