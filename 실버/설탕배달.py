5bd8d548c9c0e54ddffc5be20c0ae573b5d4d745
zpzlzmz <48462762+zpzlzmz@users.noreply.github.com>
2022-08-26 16:42:52 -0700

    Add files via upload

---
+++ 실버/설탕배달.py
@@ -0,0 +1,19 @@
+a = int(input()) #12
+
+b = a//5 # 2
+c = a%5 # 2
+
+for i in range(b+1):
+    if c == 0:
+        print(b)
+        break
+    elif c%3 != 0:
+        b -= 1
+        c += 5
+        if b == -1:
+            print(-1)
+    elif c%3 == 0:
+        print(b+(c//3))
+        break
+
+    
